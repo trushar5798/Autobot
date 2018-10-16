9c8fd8fe5a3e90dfab39283a10e3c7b60c513f64
trushar5798 <40499318+trushar5798@users.noreply.github.com>
2018-10-16 18:02:22 +0530

    Update index.js

--- index.js
+++ index.js
@@ -21,7 +21,7 @@ restService.post("/echo", function(req, res) {
       ? req.body.queryResult.parameters.q
       : "Seems like some problem. Speak again.";
   return res.json({
-    speech: speech,
+    //speech: speech,
     text: {
       "text": speech
     },
