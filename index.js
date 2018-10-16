cbc10db28a7965a9d61d64ba7e54d1313324b198
trushar5798 <40499318+trushar5798@users.noreply.github.com>
2018-10-17 01:06:39 +0530

    Update index.js

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 
+const WeatherInfo = require("./WeatherInfo.js");
+
 const restService = express();
 
 restService.use(
@@ -14,27 +16,70 @@ restService.use(
 restService.use(bodyParser.json());
 
 restService.post("/echo", function(req, res) {
-  var speech =
+  if (
     req.body.queryResult &&
     req.body.queryResult.parameters &&
     req.body.queryResult.parameters.q
-      ? req.body.queryResult.parameters.q
-      : "Seems like some problem. Speak again.";
-  return res.json({
-    fulfillmentText: "displayed&spoken response",
-    fulfillmentMessages: [
-      {
-        text: {
-          text: [
-            speech
-          ]
+  ) {
+    var speech = req.body.result.parameters.q;
+    return res.json({
+      fulfillmentMessages: [
+        {
+          text: {
+            text: [speech]
+          }
         }
-      }
-    ]
-//     //speech: speech,
-//     text: speech,
-//     source: "autobot"
-  });
+      ]
+    });
+  } else {
+    var speech = "Seems like some problem. Speak again.";
+    return res.json({
+      fulfillmentMessages: [
+        {
+          text: {
+            text: [speech]
+          }
+        }
+      ]
+    });
+  }
+  if (req.body.queryResult.action.slice(0, 7) == "weather") {
+    var address = req.body.queryResult.parameters.address.city
+      ? req.body.queryResult.parameters.address.city
+      : null;
+    address = req.body.queryResult.parameters.address.country
+      ? req.body.queryResult.parameters.address.country
+      : address;
+    address =
+      req.body.queryResult.parameters.address.admin - area.toString()
+        ? req.body.queryResult.parameters.address.admin - area.toString()
+        : address;
+    var datetime =
+      req.body.queryResult.parameters.date - time.toString()
+        ? req.body.queryResult.parameters.date - time.slice(0, 10)
+        : address;
+    var speech = getWeatherInfo(address, datetime);
+    return res.json({
+      fulfillmentMessages: [
+        {
+          text: {
+            text: [speech]
+          }
+        }
+      ]
+    });
+  } else {
+    var speech = "Seems like some problem. Speak again.";
+    return res.json({
+      fulfillmentMessages: [
+        {
+          text: {
+            text: [speech]
+          }
+        }
+      ]
+    });
+  }
 });
 
 restService.listen(process.env.PORT || 8000, function() {
