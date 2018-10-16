d848fc8130b9aead74f7889c6acafb77d6e320b6
trushar5798 <40499318+trushar5798@users.noreply.github.com>
2018-10-16 17:57:10 +0530

    Update index.js

--- index.js
+++ index.js
@@ -22,7 +22,7 @@ restService.post("/echo", function(req, res) {
       : "Seems like some problem. Speak again.";
   return res.json({
     speech: speech,
-    displayText: speech,
+    text: speech,
     source: "autobot"
   });
 });
