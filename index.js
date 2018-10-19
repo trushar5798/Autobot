68f629f3ea13d884070dfc46fd545a1efe26ac9f
trushar5798 <40499318+trushar5798@users.noreply.github.com>
2018-10-19 07:03:55 +0530

    Update index.js

--- index.js
+++ index.js
@@ -114,7 +114,7 @@ restService.post("/echo", function(req, res) {
         offset: 3   // Skip first 3 results
       }, function(error, res1, body){
 
-        ser = "<a href='"+body.webPages.value[0].url+"' target='_blank'>"+body.webPages.value[0].name+"</a>";
+        ser = body.webPages.value[0].url;
         return res.json({
           fulfillmentMessages: [
             {
